cfcfa996d955ce73cf2119e9113428401e5ade4d
Aswin CA <caaswin2001@gmail.com>
2024-01-25 09:46:29 +0530

    navbaruser

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Login from './components/Login';
 import NavbarAdmin from './components/NavbarAdmin';
 import Register from './components/Registration.js';
+import Navbaruser from './components/Navbaruser.js';
 
 function App() {
   return (
     <div>
-       <Register/>
-      
-         {/* <Login/>  */}
+       {/* <Register/> */}
+       {/* <Navbaruser/>
+         <Login/>  */}
             {/* <NavbarAdmin/> */}
+            
     </div>
   );
 }
