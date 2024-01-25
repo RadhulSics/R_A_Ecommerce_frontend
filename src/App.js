fad1ec579c6ac28b070531ce3c11b4f52135cc42
robin-abraham-ta <robinabmrbn@gmail.com>
2024-01-25 10:06:42 +0530

    banned users list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
-import NavbarAdmin from './components/NavbarAdmin';
+import NavAdmin from './components/NavbarAdmin.js';
 import Register from './components/Registration.js';
 import FullProdAdmin from './components/FullProductsAdmin.js'
+import Ban from './components/BanAdmin.js';
 
 function App() {
   return (
     <div>
        {/* <Register/> */}
          {/* <Login/>  */}
-             <NavbarAdmin/>
+             <NavAdmin/>
             <FullProdAdmin/>  
+            {/* <Ban/> */}
     </div>
   );
 }
